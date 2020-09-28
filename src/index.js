91c35066575814b964631332632192390057d022
Martin <martin.guzlej@gmail.com>
2020-09-28 12:25:50 +0100

    Faker implemented, Random names, Random text in LOREM

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+const faker = window.faker;
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam"/>
-      <CommentDetail author="Alex"/>
-      <CommentDetail author="Jane"/>
+      <CommentDetail 
+        author={faker.name.firstName()}
+        timeAgo="Today at 4:45PM" 
+        image={faker.image.avatar()}
+        text= {faker.lorem.sentence()}
+      />
+      <CommentDetail 
+        author={faker.name.firstName()}
+        timeAgo="Today at 2:00AM" 
+        image={faker.image.avatar()}
+        text={faker.lorem.sentence()}
+      />
+      <CommentDetail 
+        author={faker.name.firstName()} 
+        timeAgo="Yesterday at 5:00PM" 
+        image={faker.image.avatar()}
+        text={faker.lorem.sentence()}
+      />
     </div>
   );
 };
